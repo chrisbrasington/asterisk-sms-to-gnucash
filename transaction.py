77df59179785d7df977e10b160a0bb4273de7176
chrisbrasington <chrisbrasington@gmail.com>
2016-01-04 21:02:05 -0700

    sample transaction logic

---
+++ transaction.py
@@ -0,0 +1,38 @@
+from piecash import open_book, Transaction, Split
+from datetime import datetime
+from decimal import Decimal
+
+# reopen the book and add a transaction
+# this must be a sqlite3 file
+with open_book("/home/christopher/gnucash_backup/test.gnucash",
+               open_if_lock=True,
+               readonly=False) as mybook:
+    today = datetime.now()
+    # retrieve the currency from the book
+    USD = mybook.currencies(mnemonic="USD")
+    
+    # define the amount as Decimal
+    amount = Decimal("100.00")
+    
+    # retrieve accounts
+    to_account = mybook.accounts(fullname="Expenses:Miscellaneous")
+    from_account = mybook.accounts(fullname="Liabilities:Credit Card")
+    
+    # create the transaction with its two splits
+    Transaction(
+        post_date=today,
+        enter_date=today,
+        currency=USD,
+        description="This is a test.",
+        splits=[
+            Split(account=to_account,
+                  value=amount,
+                  memo="Split Memo!"),
+            Split(account=from_account,
+                  value=-amount,
+                  memo="Other Split Memo!"),
+        ]
+    )
+    
+    # save the book
+    mybook.save()
